0c26dd93ffe8683a89827588c83d503ffd65321a
dimasmaulanaaa <133823065+dimasmaulanaaa@users.noreply.github.com>
2023-05-25 09:24:25 +0700

    PendudukController.php

--- app/Http/Controllers/PendudukController.php
+++ app/Http/Controllers/PendudukController.php
@@ -17,7 +17,8 @@ class PendudukController extends Controller
         $penduduks = Penduduk::with('provinsi', 'kota')->paginate(10);
         $provinsis = Provinsi::all();
         $kotas = Kota::all();
-        return view('penduduk.index', compact('penduduks', 'provinsis', 'kotas'));
+        $totalData = Penduduk::count();
+        return view('penduduk.index', compact('penduduks', 'provinsis', 'kotas', 'totalData'));
     }
 
     public function search(Request $request)
@@ -38,12 +39,17 @@ class PendudukController extends Controller
         if (!empty($kota_id)) {
             $query->where('kota_id', $kota_id);
         }
+
         $penduduks = $query->with('provinsi', 'kota')->paginate(10);
+        $totalData = $query->count();
+
         $provinsis = Provinsi::all();
         $kotas = Kota::all();
 
-        return view('penduduk.index', compact('penduduks', 'provinsis', 'kotas'));
+        return view('penduduk.index', compact('penduduks', 'provinsis', 'kotas', 'totalData'));
     }
+
+
 
 
 
@@ -107,160 +113,3 @@ class PendudukController extends Controller
         return redirect()->route('penduduk.index')->with('success', 'Data penduduk berhasil dihapus.');
     }
 }
-
-    // public function getListDataPenduduk(Request $request)
-    // {
-    //     $keyword = $request->input('keyword');
-    //     $provinsiId = $request->input('provinsi_id');
-    //     $kotaId = $request->input('kota_id');
-
-    //     $query = Penduduk::query();
-
-    //     if ($keyword) {
-    //         $query->where(function ($q) use ($keyword) {
-    //             $q->where('nama', 'like', '%' . $keyword . '%')
-    //                 ->orWhere('nik', 'like', '%' . $keyword . '%');
-    //         });
-    //     }
-
-    //     if ($provinsiId) {
-    //         $query->where('provinsi_id', $provinsiId);
-    //     }
-
-    //     if ($kotaId) {
-    //         $query->where('kota_id', $kotaId);
-    //     }
-
-    //     $penduduks = $query->paginate(10);
-    //     $provinsis = Provinsi::all();
-    //     $kotas = Kota::all();
-
-    //     $totalPenduduk = Penduduk::select('provinsi_id', 'kota_id', DB::raw('count(*) as total'))
-    //         ->groupBy('provinsi_id', 'kota_id')
-    //         ->get();
-
-    //     return view('penduduk.index', compact('penduduks', 'provinsis', 'kotas', 'totalPenduduk'));
-    // }
-
-//     public function getTotal(Request $request)
-//     {
-//         $provinsi_id = $request->input('provinsi_id');
-//         $kota_id = $request->input('kota_id');
-
-//         $query = Penduduk::query();
-
-//         if ($provinsi_id) {
-//             $query->whereHas('kota', function ($q) use ($provinsi_id) {
-//                 $q->where('provinsi_id', $provinsi_id);
-//             });
-//         }
-
-//         if ($kota_id) {
-//             $query->where('kota_id', $kota_id);
-//         }
-
-//         $totalPenduduk = $query->count();
-
-//         return response()->json(['total' => $totalPenduduk]);
-//     }
-
-
-//     public function getTotalPendudukPage()
-//     {
-//         $provinsis = Provinsi::all();
-//         $kotas = Kota::all();
-
-//         return view('penduduk.total_penduduk', compact('provinsis', 'kotas'));
-//     }
-
-
-//     public function getRataUmur(Request $request)
-//     {
-//         $provinsi_id = $request->input('provinsi_id');
-//         $kota_id = $request->input('kota_id');
-
-//         $rataUmurBulan = Penduduk::select('provinsi_id', 'kota_id')
-//             ->selectRaw('AVG(umur_bulan) as rata_umur_bulan')
-//             ->where(function ($query) use ($provinsi_id, $kota_id) {
-//                 if ($provinsi_id) {
-//                     $query->where('provinsi_id', $provinsi_id);
-//                 }
-
-//                 if ($kota_id) {
-//                     $query->where('kota_id', $kota_id);
-//                 }
-//             })
-//             ->groupBy('provinsi_id', 'kota_id')
-//             ->get();
-
-//         return response()->json(['rata_umur' => $rataUmurBulan]);
-//     }
-
-
-//     public function getDataTotalPendudukByTanggalLahir(Request $request)
-//     {
-//         $bulan = $request->input('bulan');
-//         $tahun = $request->input('tahun');
-
-//         $penduduks = Penduduk::select('provinsi_id', 'kota_id', \DB::raw('count(*) as total'))
-//             ->whereMonth('tanggal_lahir', $bulan)
-//             ->whereYear('tanggal_lahir', $tahun)
-//             ->groupBy('provinsi_id', 'kota_id')
-//             ->get();
-
-//         $provinsis = Provinsi::all();
-//         $kotas = Kota::all();
-
-//         return view('penduduk.index', compact('penduduks', 'provinsis', 'kotas'));
-//     }
-
-
-//     public function getDataRataUmurBulan(Request $request)
-//     {
-//         $provinsi_id = $request->input('provinsi_id');
-//         $kota_id = $request->input('kota_id');
-
-//         $query = Penduduk::query();
-
-//         if ($provinsi_id) {
-//             $query->where('provinsi_id', $provinsi_id);
-//         }
-
-//         if ($kota_id) {
-//             $query->where('kota_id', $kota_id);
-//         }
-
-//         $penduduks = $query->get();
-
-//         $rataUmurBulan = $penduduks->avg(function ($penduduk) {
-//             return $penduduk->tanggal_lahir->diffInMonths(now());
-//         });
-
-//         $provinsis = Provinsi::all();
-//         $kotas = Kota::all();
-
-//         return view('penduduk.data_rata_umur', compact('rataUmurBulan', 'provinsis', 'kotas'));
-//     }
-// }
-  // public function getDataTotalPenduduk(Request $request)
-    // {
-    //     $total_provinsi_id = $request->input('total_provinsi_id');
-    //     $total_kota_id = $request->input('total_kota_id');
-
-    //     $query = Penduduk::query();
-
-    //     if (!empty($total_provinsi_id)) {
-    //         $query->where('provinsi_id', $total_provinsi_id);
-    //     }
-
-    //     if (!empty($total_kota_id)) {
-    //         $query->where('kota_id', $total_kota_id);
-    //     }
-
-    //     $totalPenduduk = $query->count();
-
-    //     $provinsis = Provinsi::all();
-    //     $kotas = Kota::all();
-
-    //     return view('penduduk.data_total_penduduk', compact('totalPenduduk', 'provinsis', 'kotas'));
-    // }
